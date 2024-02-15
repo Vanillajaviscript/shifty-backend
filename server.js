9381b3cd43ed73bd02184dbf1c04d845d374a284
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2024-02-15 14:23:13 -0800

    adds procfile and middleware function

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ import "dotenv/config";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import connectDB from "connections.js";
+import serverMiddleware from "./middleware/server.js";
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+dotenv.config();
 /////////
 //Middleware
 /////////
-
+const middleware = async = () => {
+  try {
+    serverMiddleware();
+  } catch(error) {
+    console.log(`Catch error: ${error}`)
+  }
+};
+middleware();
 //Root Directory
 app.get("/", (req, res) => {
   res.send("Shifty root directory")
@@ -19,6 +28,12 @@ app.get("/", (req, res) => {
 /////////
 const start = async () => {
   try {
-    
+  connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is live on port: ${PORT}`)
+  })
+  } catch(error) {
+    console.log(`Catch error: ${error}`)
   }
-}
+};
+start();
