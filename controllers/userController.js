0822100c68ce03ba3c85e4013c2b4c2588b66c2c
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2024-02-15 16:52:26 -0800

    adds user controller functions for sign in and sign up

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import UserModal from "../models/userSchema.js";
+
+const secret = process.env.SECRET;
+
+const signin = async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    const oldUser = await UserModal.findOne({username});
+    if(!oldUser) return res.status(404).json({message: "User doesn't exist"});
+      const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+      if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"});
+
+      const token = jwt.sign({username: oldUser.username, id: oldUser._id}, secret, {expiresIn: "4000h",});
+      res.status(200).json({result: oldUser, token});
+
+    } catch(error) {
+      res.status(500).json({message: "Bad Request!"});
+      console.log(error);
+  }
+};
+
+const signup = async (req, res) => {
+  const {username, email, password, firstName, lastName } = req.body;
+  try {
+    const oldUser = await UserModal.findOne({email});
+    if(oldUser) return res.status(400).json({message: "User already exists"});
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await UserModal.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign({email: result.email, id: result._id}, secret, {expiresIn: "4000h"});
+    res.status(201).json({result, token});
+  } catch(error) {
+    res.status(500).json({message: "Bad Request!"});
+    console.log(error)
+  }
+};
+
+export default {
+  signin,
+  signup
+}
