b86cab6fbf7620248e699c0c4d94a2ade6fcad9e
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2024-02-15 21:19:19 -0800

    Adds auth file to middleware folder

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/userSchema.js";
+import "dotenv/config";
+
+const secret = process.env.SECRET;
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+    let decodedData;
+    if(token && isCustomAuth) {
+      decodedData = jwt.verify(token, secret);
+      req.userID = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+      req.userId = user?._id;
+    }
+    next();
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+export default auth;
